4080cbaffbda3596870ed740aaa40632dba62306
mohd humaid <139005276+mohdhumaidd19@users.noreply.github.com>
2024-06-07 22:59:54 +0530

    Add files via upload

---
+++ backend/routes/authroutes.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { login, logout, signup ,getMe} from "../controllers/authcontrollers.js";
+import { protectRoute } from "../middleware/protectRoute.js";
+const router =express.Router();
+/// video me post hai 
+
+router.post("/signup",signup);
+router.post("/login",login);
+router.post("/logout",logout);
+router.get("/me",protectRoute,getMe);
+export default router
